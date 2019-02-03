f6b36feb2a689d0ce954ef29f1ca2c724fdb8cf8
Jason Fournier <jmf4nier@hotmail.com>
2019-02-03 17:44:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["ricky", "Bobby", "sarrah", "elia"]
+
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index} #{dwarf}"
+  end  
 end
+roll_call_dwarves(dwarves)
 
 def summon_captain_planet# code an argument here
   # Your code here
